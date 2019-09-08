df6de725b2e1e32ea43bcc665808272727c9686c
3473f <mohamedlastnameatef@gmail.com>
2019-09-08 11:43:02 +0200

    Code formatting and cleanup

--- ServoController.ino
+++ ServoController.ino
@@ -1,121 +1,139 @@
-//GRBL Pins
-#define STEP 18 //ISR
-#define DIR 19
+/* DEFINES ********************************************************************/
+// GRBL Pins
+#define STEP  18
+#define DIR   19
 
 // Encoder Pins
-#define encoderPinA 2 //ISR
-#define encoderPinB 3 //ISR
+#define ENCODER_CH_A  2
+#define ENCODER_CH_B  3
 
 // Motor Driver Pins
-#define mSpeed 8
-#define mDir 9
+#define MOTOR_SPEED  8
+#define MOTOR_DIR    9
 
-volatile long encoderCount = 0;
-volatile long stepsCount = 0;
-long targetCounts = 0;
+// PID Anti-windup Term
+#define I_MAX   10
+#define I_MIN  -10
 
-// PID
-float Kp, Ki, Kd;
-float prevError, iError;
-float pTerm, iTerm, dTerm;
-long error;
-long previousMillis = 0;
-long interval = 5;
-int motorSpeed;
-int iMin = -10;
-int iMax = 10;
+// PID Loop Interval
+#define SAMPLING_INTERVAL 5
 
+/* GLOBAL VARIABLES ***********************************************************/
+volatile long encoderCount = 0; // number of pulses from encoder
+volatile long stepsCount = 0;   // target count for the motor
 
+/* SETUP CODE *****************************************************************/
 void setup() {
   // Motor Driver Pins
-  pinMode(mSpeed, OUTPUT);
-  pinMode(mDir, OUTPUT);
+  pinMode(MOTOR_SPEED, OUTPUT);
+  pinMode(MOTOR_DIR, OUTPUT);
 
   // GRBL Pins
   pinMode(STEP, INPUT);
   pinMode(DIR, INPUT);
 
   // Encoder Pins
-  pinMode(encoderPinA, INPUT);
-  pinMode(encoderPinB, INPUT);
+  pinMode(ENCODER_CH_A, INPUT);
+  pinMode(ENCODER_CH_B, INPUT);
 
   // turn on pull-up resistors
-  digitalWrite(encoderPinA, HIGH);
-  digitalWrite(encoderPinB, HIGH);
+  digitalWrite(ENCODER_CH_A, HIGH);
+  digitalWrite(ENCODER_CH_B, HIGH);
 
   // tun on pull-down resistors
   digitalWrite(STEP, LOW);
 
   // Interrupts
-  attachInterrupt(digitalPinToInterrupt(encoderPinA), doEncoderA, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(encoderPinB), doEncoderB, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_CH_A), doEncoderA, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_CH_B), doEncoderB, CHANGE);
   attachInterrupt(digitalPinToInterrupt(STEP), stepCount, RISING);
-
-  // Controller Gains
-  Kp = 2.22;
-  Ki = 3.5;
-  Kd = 0.38;
 }
 
+/* MAIN LOOP ******************************************************************/
 void loop() {
-  targetCounts = stepsCount;
-  doPID();
+  long targetCounts = stepsCount;
+  doPID(targetCounts);
 }
 
-void doPID(){
-  if (millis() - previousMillis > interval){
+/* FUNCTION DEFINITION ********************************************************/
+/*
+ * PID Controller function
+ */
+void doPID(long targetCounts){
+  static long previousMillis = 0; // last loop time
+  static float prevError;         // last control error
+  
+  // Controller gains
+  float Kp = 2.22;
+  float Ki = 3.5;
+  float Kd = 0.38;
+  
+  float pTerm, iTerm, dTerm;
+  long error;
+  long iError;
+  int motorSpeed;
+  
+  if (millis() - previousMillis > SAMPLING_INTERVAL){
     previousMillis = millis();
 
     error = encoderCount - targetCounts;
     pTerm = Kp * error;
     iError += error;
-    iTerm = Ki * constrain(iError, iMin, iMax);
+    iTerm = Ki * constrain(iError, I_MIN, I_MAX);
     dTerm = Kd * (error - prevError);
 
     motorSpeed = constrain(pTerm + iTerm + dTerm, -255, 255);
 
     if(motorSpeed > 0){
-      digitalWrite(mDir, HIGH);
+      digitalWrite(MOTOR_DIR, HIGH);
     }
     if(motorSpeed < 0){
-      digitalWrite(mDir, LOW);
+      digitalWrite(MOTOR_DIR, LOW);
       motorSpeed = -1 * motorSpeed;
     }
-    analogWrite(mSpeed, motorSpeed);
+    analogWrite(MOTOR_SPEED, motorSpeed);
   }
 }
 
-// Interrput Service Routines
+/*
+ * ISR for encoder channel A
+ */
 void doEncoderA(){
-  if(digitalRead(encoderPinA) == HIGH){
-    if(digitalRead(encoderPinB) == LOW)
+  if(digitalRead(ENCODER_CH_A) == HIGH){
+    if(digitalRead(ENCODER_CH_B) == LOW)
       encoderCount += 1;
     else
       encoderCount -= 1;
   }
   else{
-    if(digitalRead(encoderPinB) == HIGH)
+    if(digitalRead(ENCODER_CH_B) == HIGH)
       encoderCount += 1;
     else
       encoderCount -= 1;
   }
 }
 
+/*
+ * ISR for encoder channel B
+ */
 void doEncoderB(){
-  if(digitalRead(encoderPinB) == HIGH){
-    if(digitalRead(encoderPinA) == HIGH)
+  if(digitalRead(ENCODER_CH_B) == HIGH){
+    if(digitalRead(ENCODER_CH_A) == HIGH)
       encoderCount += 1;
     else
       encoderCount -= 1;
   }
   else{
-    if(digitalRead(encoderPinA) == LOW)
+    if(digitalRead(ENCODER_CH_A) == LOW)
       encoderCount += 1;
     else
       encoderCount -= 1;
   }
 }
 
+/*
+ * ISR to count the steps required for the motor
+ */
 void stepCount(){
   if(digitalRead(DIR))
     stepsCount++;
